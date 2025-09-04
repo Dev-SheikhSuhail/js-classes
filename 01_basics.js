8960b491b612b07c4ce93b8382c5507ad8555e9b
SheikhSuhail <dev.sheikhsuhail@gmail.com>
2025-09-04 08:42:15 +0530

    JavaScript Basics

---
+++ 01_basics.js
@@ -0,0 +1,19 @@
+// let myVar = '';
+// let var2 = null;
+// let var3 = undefined;
+// let var4;
+
+// console.table([myVar, var2, var3]);
+// console.table([typeof myVar, typeof var2, typeof var3, typeof var4]);
+
+// let myNum = 455;
+
+// let str = String(myNum);
+// console.table([typeof myNum, typeof str]);
+// console.log(str);
+
+// let myStr = '4456Sahil';
+// console.log(typeof myStr);
+
+// let num2 = Number(myStr);
+// console.table([num2, typeof num2]);
