46442d1bb174525c4729c5e446d2ae2efe527854
SheikhSuhail <dev.sheikhsuhail@gmail.com>
2025-09-04 08:45:44 +0530

    Math in JS

---
+++ 02_math.js
@@ -0,0 +1,20 @@
+// **********MATH***********
+console.log(Math.abs(-5));
+console.log(Math.abs(+5));
+console.log(Math.abs(5));
+
+console.log(Math.round(5.8)); //6
+console.log(Math.round(5.2)); //5
+console.log(Math.round(5.5)); //6
+console.log(Math.round(5)); //5
+
+console.log(Math.floor(5.8));
+console.log(Math.floor(5.2));
+console.log(Math.floor(5));
+
+console.log(Math.ceil(5.8));
+console.log(Math.ceil(5.2));
+console.log(Math.ceil(5));
+console.log(Math.max(4, 8, 13, 88));
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10));
